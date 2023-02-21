f30fa9b2469b6c0284876218fc1f0b78d6cd9f60
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2023-02-21 10:00:05 +0100

    gitautosync update 5

--- data-carpentry/scripts/intro-r.R
+++ data-carpentry/scripts/intro-r.R
@@ -4,3 +4,7 @@ dir.create("fig_output")
 dir.create("scripts")
 download.file("https://ndownloader.figshare.com/files/11492171", "data/SAFI_clean.csv", mode = "wb")
 install.packages("tidyverse")
+
+area_hectares <- 1.0 # assign 1.0 to an object called area_hectares
+
+2.47 * area_hectares
