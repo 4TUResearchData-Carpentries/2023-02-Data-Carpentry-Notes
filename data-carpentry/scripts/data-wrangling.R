cb9f4706bce76b3058dbd7eeb2b6ac0924d34cf5
alwil <alexandra.wilczynska@gmail.com>
2023-02-23 12:51:12 +0100

    gitautosync update 35

--- data-carpentry/scripts/data-wrangling.R
+++ data-carpentry/scripts/data-wrangling.R
@@ -146,3 +146,13 @@ interviews_wide <- interviews %>%
               values_from = wall_type_logical, 
               values_fill = FALSE )
 view(interviews_wide)
+
+# convert from wide to long 
+
+interviews_long <- interviews_wide %>%
+  pivot_longer(cols = c(muddaub, cement, sunbricks, burntbricks),
+               names_to = "respondent_wall_type",
+               values_to = "wall_type_logical"
+               ) %>% 
+  filter(wall_type_logical == TRUE) %>%
+  select(-wall_type_logical)
